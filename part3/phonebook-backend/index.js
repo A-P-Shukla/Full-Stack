01ac8cffa6e5496b5b7f498a70a3079994c53b59
A-P-Shukla <akhandshukla36@gmail.com>
2026-03-19 09:20:53 +0530

    Add phonebook backend with CRUD and validation

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+let persons = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
+]
+
+const generateId = () => Math.floor(Math.random() * 1000000).toString()
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const info = `Phonebook has info for ${persons.length} people`
+  const time = new Date().toString()
+  response.send(`<p>${info}</p><p>${time}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find((entry) => entry.id === id)
+
+  if (!person) {
+    return response.status(404).end()
+  }
+
+  return response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter((entry) => entry.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number is missing' })
+  }
+
+  const nameExists = persons.some((entry) => entry.name === body.name)
+  if (nameExists) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  return response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
